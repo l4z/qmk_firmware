c16742d6536cddfc583a8e063b3b3e8c059611a6
l4z <lacos83@gmail.com>
2025-01-16 20:34:42 +0100

    move everything to my keymap file

--- keyboards/hotdox76v2/hotdox76v2.c
+++ keyboards/hotdox76v2/hotdox76v2.c
@@ -8,17 +8,6 @@
 #include "oled_font_lib/logo2.h"
 #include "oled_font_lib/ext_font.h"
 
-typedef enum {
-    BASE,
-    SYMBOLS,
-    FN,
-    CONTROL
-} my_layers_t;
-
-void keyboard_post_init_user(void) {
-    layer_on(SYMBOLS);
-}
-
 #ifdef RGB_MATRIX_ENABLE
 // clang-format off
 led_config_t g_led_config = {
@@ -67,194 +56,123 @@ led_config_t g_led_config = {
 };
 // clang-format on
 
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-	// rgb_config_t rgb_matrix_config;
-
-    if (rgb_matrix_config.mode > 1) {
-        return true;
-    }
-
-    // fav hue 23 * 8
-    // nav for fav hue 2 * 8
-    const hsv_t hsv = rgb_matrix_config.hsv;
-    const rgb_t rgb = hsv_to_rgb(hsv);
-
-    const rgb_t hl_rgb = hsv_to_rgb((hsv_t){hsv.h, 0, hsv.v});
-    const rgb_t desat_rgb = hsv_to_rgb((hsv_t){hsv.h, hsv.s - 8*RGB_MATRIX_SAT_STEP, hsv.v});
-    const rgb_t nav_rgb = hsv_to_rgb((hsv_t){hsv.h - 21*RGB_MATRIX_HUE_STEP, hsv.s, hsv.v});
-    const rgb_t ctrl_rgb = hsv_to_rgb((hsv_t){hsv.h - 3*RGB_MATRIX_HUE_STEP, hsv.s, hsv.v});
-
-    // numbers: 1 <= i < 7; 46 <= i < 50
-    // FN keys: 1 <= i < 7; 44 <= i < 50
-    // FN keys 2nd row: 8 <= i < 13; 51 <= i < 56
-	// arrows cluster: 68, 70, 71, 72
-	// underglow left: 38 <= i < 43
-	// underglow right: 81 <= i < 86
-    // thumb cluster left: 32 <= i < 38 // starts at 'space', goes to bottom corner, then far corner, finally 1 up
-    // thumb cluster right: 75 <= i < 81 // starts at 'space', goes to bottom corner, then far corner, finally 1 up
-
-    for (int i = led_min; i < led_max; i++) {
-        rgb_t i_color = rgb;
-
-        if ((i == 0 || (i >= 34 && i < 38) || (i >= 75 && i < 81)) && layer_state & (1 << CONTROL)) {
-            i_color = ctrl_rgb;
-        } else if ((i >= 1 && i < 7) || (i >= 46 && i < 50)) {
-            if (layer_state & (1 << FN)) {
-                i_color = hl_rgb;
-            } else if (layer_state & (1 << SYMBOLS)) {
-                i_color = nav_rgb;
-            } else {
-                i_color = desat_rgb;
-            }
-        } else if (i >= 44 && i < 46 && layer_state & (1 << FN)) {
-            i_color = hl_rgb;
-        // arrows
-        } else if (i == 68 || i == 70 || i == 71 || i == 72) {
-            i_color = nav_rgb;
-        }
-        RGB_MATRIX_INDICATOR_SET_COLOR(i, i_color.r, i_color.g, i_color.b);
-    }
-
-    return false;
-}
-
 #endif
 
 #ifdef OLED_ENABLE
 
 #    define UNC (' ')
 
-typedef enum {
-	KEY_OVERRIDES,
-    DT_TERM
-} state_to_show_t;
+//typedef enum {
+//	KEY_OVERRIDES,
+//    DT_TERM
+//} state_to_show_t;
+//
+//state_to_show_t state_to_show;
+//
+//oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
+//
+//	state_to_show = KEY_OVERRIDES;
+//
+//    if (is_keyboard_left()) {
+//        return OLED_ROTATION_180;
+//    } else {
+//        return OLED_ROTATION_0;
+//    }
+//}
 
-typedef struct _master_to_slave_t {
-    state_to_show_t  state_to_show;
-    bool ko_enabled;
-    uint16_t dt;
-} master_to_slave_t;
-master_to_slave_t m2s;
-master_to_slave_t s2m;
+//void oled_show(uint8_t start_line, const char *data, uint8_t gap_w, uint8_t l, uint8_t actual_length, bool read_data_from_progmem) {
+//    uint8_t j = 0, k = 0;
+//    uint8_t logo_w = 0; // used to be 32 for alpacca logo
+//    for (j = 0; j < l; ++j) {      // char index
+//        const char character = read_data_from_progmem ? pgm_read_byte(&data[j]) : data[j];
+//        const unsigned char *character_font;
+//        if (j < actual_length) {
+//        	character_font = ext_big_font[character - 0x20];
+//        } else {
+//        	character_font = ext_big_font[0]; // whitespace
+//        }
+//        for (k = 0; k < 12; ++k) { // font pixel column byte index, each
+//            // base + logo_w(32) + gap_w(12) +l*font_w(12)+current_byte_index
+//            oled_write_raw_byte(pgm_read_byte(&character_font[k]), start_line * 2 * 128 + logo_w + gap_w + j * 12 + k);
+//            oled_write_raw_byte(pgm_read_byte(&character_font[k + 12]), start_line * 2 * 128 + 128 + logo_w + gap_w + j * 12 + k);
+//        }
+//    }
+//    for (j = 0; j < gap_w; ++j) {
+//        oled_write_raw_byte(pgm_read_byte(&blank_block), start_line * 2 * 128 + logo_w + j);
+//        oled_write_raw_byte(pgm_read_byte(&blank_block), start_line * 2 * 128 + logo_w + gap_w + l * 12 + j);
+//
+//        oled_write_raw_byte(pgm_read_byte(&blank_block), start_line * 2 * 128 + 128 + logo_w + j);
+//        oled_write_raw_byte(pgm_read_byte(&blank_block), start_line * 2 * 128 + 128 + logo_w + gap_w + l * 12 + j);
+//    }
+//}
 
-oled_rotation_t oled_init_kb(oled_rotation_t rotation) {
+//void render_layer(uint8_t layer) {
+//    uint8_t gap_w = 2;
+//    uint8_t len = 10;
+//    oled_show(0, PSTR("LAYER:"), gap_w, len, 6, true);
+//	// with defaut font (half height): oled_write_P(PSTR("Layer: "), false);
+//    switch (layer) {
+//        case 0:
+//            oled_show(1, PSTR("0:BASE"), gap_w, len, 6, true);
+//			break;
+//        case 1:
+//            oled_show(1, PSTR("1:SYMBOLS"), gap_w, len, 9, true);
+//			break;
+//        case 2:
+//            oled_show(1, PSTR("2:FN"), gap_w, len, 4, true);
+//            break;
+//        case 3:
+//            oled_show(1, PSTR("3:CONTROL"), gap_w, len, 9, true);
+//            break;
+//        case 4:
+//            oled_show(1, PSTR("4:UNUSED"), gap_w, len, 8, true);
+//            break;
+//        default:
+//            oled_show(1, PSTR("UNKNOWN"), gap_w, len, 7, true);
+//            break;
+//    }
+//}
 
-	m2s.state_to_show = KEY_OVERRIDES;
-	m2s.ko_enabled = true;
-    m2s.dt = TAPPING_TERM;
-	s2m.state_to_show = KEY_OVERRIDES;
-	s2m.ko_enabled = true;
-    s2m.dt = TAPPING_TERM;
+//void render_kb_state(void) {
+//    uint8_t len = 10;
+//    uint8_t gap_w = 2;
+//    switch (state_to_show) {
+//        case KEY_OVERRIDES:
+//            bool en = key_override_is_enabled();
+//            oled_show(0, PSTR("KEY OVRDS:"), gap_w, len, 10, true);
+//            if (en) {
+//            	oled_show(1, PSTR("ON"), gap_w + 10, len - 1, 2, true);
+//            } else {
+//            	oled_show(1, PSTR("OFF"), gap_w + 10, len - 1, 3, true);
+//            }
+//            return;
+//#    ifdef DYNAMIC_TAPPING_TERM_ENABLE
+//        case DT_TERM:
+//            oled_show(0, PSTR("DT TERM:"), gap_w, len, 8, true);
+//    		static char value_line[9];
+//			int actual_length = sprintf(value_line, "%dms", g_tapping_term);
+//            oled_show(1, value_line, gap_w + 10, len - 1, actual_length, false);
+//            return;
+//#    endif
+//        default:
+//            oled_show(0, PSTR("!Unhandled"), gap_w, len, 10, true);
+//            oled_show(1, PSTR(""), gap_w, len, 0, true);
+//            return;
+//    }
+//}
 
-    if (is_keyboard_left()) {
-        return OLED_ROTATION_180;
-    } else {
-        return OLED_ROTATION_0;
-    }
-}
-
-void oled_show(uint8_t start_line, const char *data, uint8_t gap_w, uint8_t l, uint8_t actual_length, bool read_data_from_progmem) {
-    uint8_t j = 0, k = 0;
-    uint8_t logo_w = 0; // used to be 32 for camel logo
-    for (j = 0; j < l; ++j) {      // char index
-        const char character = read_data_from_progmem ? pgm_read_byte(&data[j]) : data[j];
-        const unsigned char *character_font;
-        if (j < actual_length) {
-        	character_font = ext_big_font[character - 0x20];
-        } else {
-        	character_font = ext_big_font[0]; // whitespace
-        }
-        for (k = 0; k < 12; ++k) { // font pixel column byte index, each
-            // base + logo_w(32) + gap_w(12) +l*font_w(12)+current_byte_index
-            oled_write_raw_byte(pgm_read_byte(&character_font[k]), start_line * 2 * 128 + logo_w + gap_w + j * 12 + k);
-            oled_write_raw_byte(pgm_read_byte(&character_font[k + 12]), start_line * 2 * 128 + 128 + logo_w + gap_w + j * 12 + k);
-        }
-    }
-    for (j = 0; j < gap_w; ++j) {
-        oled_write_raw_byte(pgm_read_byte(&blank_block), start_line * 2 * 128 + logo_w + j);
-        oled_write_raw_byte(pgm_read_byte(&blank_block), start_line * 2 * 128 + logo_w + gap_w + l * 12 + j);
-
-        oled_write_raw_byte(pgm_read_byte(&blank_block), start_line * 2 * 128 + 128 + logo_w + j);
-        oled_write_raw_byte(pgm_read_byte(&blank_block), start_line * 2 * 128 + 128 + logo_w + gap_w + l * 12 + j);
-    }
-}
-
-void render_layer(uint8_t layer) {
-    uint8_t gap_w = 2;
-    uint8_t len = 10;
-    oled_show(0, PSTR("LAYER:"), gap_w, len, 6, true);
-	// with defaut font (half height): oled_write_P(PSTR("Layer: "), false);
-    switch (layer) {
-        case 0:
-            oled_show(1, PSTR("0:BASE"), gap_w, len, 6, true);
-			break;
-        case 1:
-            oled_show(1, PSTR("1:SYMBOLS"), gap_w, len, 9, true);
-			break;
-        case 2:
-            oled_show(1, PSTR("2:FN"), gap_w, len, 4, true);
-            break;
-        case 3:
-            oled_show(1, PSTR("3:CONTROL"), gap_w, len, 9, true);
-            break;
-        case 4:
-            oled_show(1, PSTR("4:UNUSED"), gap_w, len, 8, true);
-            break;
-        default:
-            oled_show(1, PSTR("UNKNOWN"), gap_w, len, 7, true);
-            break;
-    }
-}
-
-void render_kb_state_tpe(master_to_slave_t sync_data) {
-    uint8_t len = 10;
-    uint8_t gap_w = 2;
-    switch (sync_data.state_to_show) {
-        case KEY_OVERRIDES:
-            bool en = sync_data.ko_enabled;
-            oled_show(0, PSTR("KEY OVRDS:"), gap_w, len, 10, true);
-            if (en) {
-            	oled_show(1, PSTR("ON"), gap_w + 10, len - 1, 2, true);
-            } else {
-            	oled_show(1, PSTR("OFF"), gap_w + 10, len - 1, 3, true);
-            }
-            return;
-#    ifdef DYNAMIC_TAPPING_TERM_ENABLE
-        case DT_TERM:
-            oled_show(0, PSTR("DT TERM:"), gap_w, len, 8, true);
-    		static char value_line[9];
-			int actual_length = sprintf(value_line, "%dms", sync_data.dt);
-            oled_show(1, value_line, gap_w + 10, len - 1, actual_length, false);
-            return;
-#    endif
-        default:
-            oled_show(0, PSTR("!Unhandled"), gap_w, len, 10, true);
-            oled_show(1, PSTR(""), gap_w, len, 0, true);
-            return;
-    }
-}
-
-void render_kb_state(void) {
-    if (is_keyboard_master()) {
-        render_kb_state_tpe(m2s);
-    } else {
-        render_kb_state_tpe(s2m);
-    }
-}
-
-bool oled_task_kb(void) {
-    if (!oled_task_user()) {
-        return false;
-    }
-    if (is_keyboard_left()) {
-        // layer_state is uint32_t
+//bool oled_task_kb(void) {
+//    if (!oled_task_user()) {
+//        return false;
+//    }
+//    if (is_keyboard_master()) {
 //        render_kb_state();
-        render_layer(biton32(layer_state));
-    } else {
-        render_kb_state();
-    }
-    return false;
-}
+//    } else {
+//        // layer_state is uint32_t
+//        render_layer(biton32(layer_state));
+//    }
+//    return false;
+//}
 
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_user(keycode, record)) {
@@ -276,70 +194,28 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-//void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KO_TOGG:
-        case KO_ON:
-        case KO_OFF:
-            if (record->event.pressed) {
-                bool ko_enabled = keycode == KO_TOGG ? !key_override_is_enabled() : key_override_is_enabled();
-            	m2s.state_to_show = KEY_OVERRIDES;
-                m2s.ko_enabled = ko_enabled;
-            }
-            return true;
-#    ifdef DYNAMIC_TAPPING_TERM_ENABLE
-        case DT_PRNT:
-        case DT_UP:
-        case DT_DOWN:
-            if (record->event.pressed) {
-                uint16_t dt = g_tapping_term;
-                if (keycode == DT_UP) {
-                	dt += DYNAMIC_TAPPING_TERM_INCREMENT;
-                } else if (keycode == DT_DOWN) {
-                    dt -= DYNAMIC_TAPPING_TERM_INCREMENT;
-                }
-            	m2s.state_to_show = DT_TERM;
-                m2s.dt = dt;
-            }
-            return true;
-#    endif
-        default:
-            return true;
-    }
-}
-
-void user_sync_alpa_slave_handler(uint8_t in_buflen, const void *in_data, uint8_t out_buflen, void *out_data) {
-    const master_to_slave_t *m2s_p = (const master_to_slave_t *)in_data;
-    s2m.state_to_show = m2s_p->state_to_show;
-    s2m.ko_enabled = m2s_p->ko_enabled;
-    s2m.dt = m2s_p->dt;
-}
-
-void keyboard_post_init_kb(void) {
-    transaction_register_rpc(KEYBOARD_CURRENT_ALPA_SYNC, user_sync_alpa_slave_handler);
-    keyboard_post_init_user();
-}
-
-void housekeeping_task_kb(void) {
-    if (is_keyboard_master()) {
-        if (!is_oled_on()) {
-            m2s.state_to_show = KEY_OVERRIDES;
-            m2s.ko_enabled = true;
-            m2s.dt = TAPPING_TERM;
-        }
-        // Interact with slave every 200ms
-        static uint32_t last_sync = 0;
-        if (timer_elapsed32(last_sync) > 200) {
-            if (transaction_rpc_exec(KEYBOARD_CURRENT_ALPA_SYNC, sizeof(m2s), &m2s, sizeof(s2m), &s2m)) {
-                last_sync = timer_read32();
-                dprint("Slave sync successed!\n");
-            } else {
-                dprint("Slave sync failed!\n");
-            }
-        }
-    }
-}
+//bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//    switch (keycode) {
+//        case KO_TOGG:
+//        case KO_ON:
+//        case KO_OFF:
+//            if (record->event.pressed) {
+//            	state_to_show = KEY_OVERRIDES;
+//            }
+//            return true;
+//#    ifdef DYNAMIC_TAPPING_TERM_ENABLE
+//        case DT_PRNT:
+//        case DT_UP:
+//        case DT_DOWN:
+//            if (record->event.pressed) {
+//            	state_to_show = DT_TERM;
+//            }
+//            return true;
+//#    endif
+//        default:
+//            return true;
+//    }
+//}
 
 #endif
 
--- keyboards/hotdox76v2/keymaps/l4z/readme.md
+++ keyboards/hotdox76v2/keymaps/l4z/readme.md
@@ -16,6 +16,10 @@ Flashing example for this keyboard:
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
 
+Flashing left/right:
+
+    qmk flash -bl dfu-split-left
+    qmk flash -bl dfu-split-right
 
 ## Bootloader
 
