077a3c5e7ef08f422018191f2c68c1123383309b
l4z <lacos83@gmail.com>
2025-09-18 08:06:13 +0200

    thumb cluster adjusments, oled display shows rgb settings

--- keyboards/hotdox76v2/keymaps/l4z/keymap.c
+++ keyboards/hotdox76v2/keymaps/l4z/keymap.c
@@ -34,20 +34,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                              │   │   │Del│ │Ctl│   │   │
      *                              └───┴───┴───┘ └───┴───┴───┘
      */
-    [0] = LAYOUT_ergodox_pretty(
+    [BASE] = LAYOUT_ergodox_pretty(
 //        KC_ESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC,           KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,  KC_EQL, KC_BSPC,
         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,              KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LCBR,           KC_RCBR,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
-        KC_GRV,MT(MOD_LSFT,KC_A),MT(MOD_LCTL,KC_S),MT(MOD_LALT,KC_D),LT(2,KC_F),LT(3,KC_G),
-        											   LT(3,KC_H),LT(2,KC_J),MT(MOD_RALT,KC_K),MT(MOD_RCTL,KC_L),MT(MOD_RSFT,KC_SCLN),KC_BSLS,
+        KC_GRV,MT(MOD_LSFT,KC_A),MT(MOD_LCTL,KC_S),MT(MOD_LALT,KC_D),LT(FN,KC_F),MT(MOD_LGUI,KC_G),
+        											   MT(MOD_RGUI,KC_H),LT(FN,KC_J),MT(MOD_RALT,KC_K),MT(MOD_RCTL,KC_L),MT(MOD_RSFT,KC_SCLN),KC_BSLS,
        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LPRN,           KC_RPRN,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP,  KC_ENT,
-          KC_LCTL, KC_LGUI, KC_LALT,  MO(2),  MO(3),                                            KC_QUOT, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT,
-                                                     KC_LBRC, KC_F18,            KC_F18, KC_RBRC,
-                                                              KC_F19,            KC_F19,
-                                             KC_SPC, KC_BSPC, KC_F20,            KC_F20, KC_ENT, KC_SPC
+          KC_LCTL, KC_LGUI, KC_LALT,  MO(FN),  MO(CONTROL),                                            KC_QUOT, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT,
+                                                     KC_LBRC, KC_F22,            KC_F22, KC_RBRC,
+                                                              KC_F23,            KC_F23,
+                                             KC_SPC, KC_LSFT, KC_F24,            KC_F24, KC_BSPC, KC_SPC
     ),
 
-    [1] = LAYOUT_ergodox_pretty(
+    [SYMBOLS] = LAYOUT_ergodox_pretty(
 //       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,               KC_7,    KC_8,    KC_9,    KC_0, _______, _______, _______,
        _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC,            KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______, _______,
        _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
@@ -59,33 +59,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            _______, _______, _______,            _______, _______, _______
     ),
 
-    [2] = LAYOUT_ergodox_pretty(
-         TG(1),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,              KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_F11,
-       _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______,  KC_F12,
-       _______, KC_LSFT, KC_LCTL, KC_LALT,   MO(2),   MO(4),                                MO(4),   MO(2), KC_RALT, KC_RCTL, KC_RSFT, KC_PSCR,
+    [FN] = LAYOUT_ergodox_pretty(
+         TG(SYMBOLS),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,              KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,
+       _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
+       _______, KC_LSFT, KC_LCTL, KC_LALT,   MO(FN),   MO(CONTROL),                               MO(CONTROL),   MO(FN), KC_RALT, KC_RCTL, KC_RSFT, KC_PSCR,
        _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, KC_PGUP, _______,
-          _______, _______, _______,  MO(2),  MO(4),                                            _______, _______, KC_HOME, KC_PGDN,  KC_END,
+          _______, _______, _______,  MO(FN),  MO(CONTROL),                                            _______, _______, KC_HOME, KC_PGDN,  KC_END,
                                                     _______, _______,            _______, _______,
                                                              _______,            _______,
                                            _______, _______, _______,            _______, _______, _______
     ),
 
-    [3] = LAYOUT_ergodox_pretty(
+    [CONTROL] = LAYOUT_ergodox_pretty(
          TG(1), _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
        _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
-       _______, KC_LSFT, KC_LCTL, KC_LALT,   MO(3),   MO(4),                                MO(4),   MO(3), KC_RALT, KC_RCTL, KC_RSFT, _______,
+       _______, KC_LSFT, KC_LCTL, KC_LALT,   MO(FN),   MO(CONTROL),                          MO(CONTROL), MO(FN), KC_RALT, KC_RCTL, KC_RSFT, _______,
        _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
-          _______, _______, _______,    MO(3),   MO(4),                                         _______, _______, _______, _______, _______,
-                                                    KO_TOGG, DT_PRNT,            RM_TOGG, RM_NEXT,
+          _______, _______, _______,    MO(FN),   MO(CONTROL),                                         _______, _______, _______, _______, _______,
+                                                    KO_TOGG, RM_TOGG,            RM_SPDU, RM_NEXT,
                                                                DT_UP,            RM_SATU,
                                            _______, _______, DT_DOWN,            RM_HUEU, RM_VALD, RM_VALU
     )
+
+//    [UNICODE] = LAYOUT_ergodox_pretty(
+//       _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, UP(0x00F6,0x00D6), UP(0x00FC,0x0170), UP(0x00F3,0x00D3), _______,
+//       _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
+//       _______, KC_LSFT, KC_LCTL, KC_LALT,   MO(UNICODE),   MO(UNICODE),                        _______, _______, _______, _______, _______, _______,
+//       _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, _______,
+//          _______, _______, _______,    MO(UNICODE),   MO(UNICODE),                                  _______, _______, _______, _______, _______,
+//                                                    _______, _______,            _______, _______,
+//                                                             _______,            _______,
+//                                           _______, _______, _______,            _______, _______, _______
+//    )
 };
 
 #ifdef KEY_OVERRIDE_ENABLE
 
-const key_override_t l_paren_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_LPRN, KC_LT, 1);
-const key_override_t r_paren_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_RPRN, KC_GT, 1);
+const key_override_t l_paren_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_LPRN, KC_LT, SYMBOLS);
+const key_override_t r_paren_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_RPRN, KC_GT, SYMBOLS);
 
 const key_override_t l_brace_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LCBR, KC_LBRC);
 const key_override_t r_brace_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_RCBR, KC_RBRC);
@@ -98,8 +109,8 @@ const key_override_t kc_5_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_PERC, 
 const key_override_t kc_6_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_CIRC, KC_6);
 const key_override_t kc_7_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_AMPR, KC_7);
 const key_override_t kc_8_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_ASTR, KC_8);
-const key_override_t kc_9_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_LPRN, KC_9, ~1);
-const key_override_t kc_0_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_RPRN, KC_0, ~1);
+const key_override_t kc_9_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_LPRN, KC_9, ~SYMBOLS);
+const key_override_t kc_0_key_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_RPRN, KC_0, ~SYMBOLS);
 
 const key_override_t *key_overrides[] = {
 	&l_paren_key_override,
@@ -124,7 +135,7 @@ const key_override_t *key_overrides[] = {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     // see: rgb_config_t rgb_matrix_config;
 
-    if (rgb_matrix_config.mode > 1) {
+    if (rgb_matrix_config.mode != 1) {
         return true;
     }
 
@@ -135,12 +146,13 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
     // saturation variations
     const rgb_t white_rgb = hsv_to_rgb((hsv_t){hsv.h, 0, hsv.v});
-    const rgb_t desat_rgb = hsv_to_rgb((hsv_t){hsv.h, hsv.s / 2 + 32, hsv.v});
     
     // hue variations
     const rgb_t opp_rgb = hsv_to_rgb((hsv_t){hsv.h - 107, hsv.s, hsv.v});
     const rgb_t less_rgb = hsv_to_rgb((hsv_t){hsv.h - 43, hsv.s, hsv.v});
     const rgb_t more_rgb = hsv_to_rgb((hsv_t){hsv.h + 107, hsv.s, hsv.v});
+    
+    const rgb_t homerow_rgb = hsv_to_rgb((hsv_t){hsv.h - 16, hsv.s, hsv.v});
 
     const uint8_t mod_state = get_mods();
 
@@ -170,7 +182,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         } else if (i >= 44 && i < 46 && layer_state & (1 << FN)) {
             i_color = white_rgb;
         } else if ((i >= 15 && i < 20) || (i >= 58 && i < 63)) {
-            i_color = desat_rgb;
+            i_color = homerow_rgb;
         } else if (i == 68 || i == 70 || i == 71 || i == 72) {
             i_color = opp_rgb;
         }
@@ -184,15 +196,20 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
 #ifdef OLED_ENABLE
 typedef enum {
+    SYMBOLS_ACTIVE,
     KEY_OVERRIDES,
-    DT_TERM
+    DT_TERM,
+    LED_MATRIX_HUE,
+    LED_MATRIX_SATURATION,
+    LED_MATRIX_VOLUME,
+    LED_MATRIX_MODE
 } state_to_show_t;
 
 state_to_show_t state_to_show;
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 
-    state_to_show = KEY_OVERRIDES;
+    state_to_show = SYMBOLS_ACTIVE;
 
     if (is_keyboard_left()) {
         return OLED_ROTATION_180;
@@ -227,15 +244,51 @@ void oled_show(uint8_t start_line, const char *data, uint8_t gap_w, uint8_t l, u
     }
 }
 
+static const uint8_t len = 10;
+static const uint8_t gap_w = 2;
+
+uint8_t led_matrix_state_value(void) {
+	switch (state_to_show) {
+        case LED_MATRIX_HUE:
+            oled_show(0, PSTR("LED HUE:"), gap_w, len, 8, true);
+            return rgb_matrix_config.hsv.h;
+		case LED_MATRIX_SATURATION:
+            oled_show(0, PSTR("LED SAT:"), gap_w, len, 8, true);
+            return rgb_matrix_config.hsv.s;
+        case LED_MATRIX_VOLUME:
+            oled_show(0, PSTR("LED VOL:"), gap_w, len, 8, true);
+            return rgb_matrix_config.hsv.v;
+        case LED_MATRIX_MODE:
+            oled_show(0, PSTR("LED MODE:"), gap_w, len, 9, true);
+            return rgb_matrix_config.mode;
+        default:
+            return 0;
+    }
+}
+
 void render_kb_state(void) {
-    uint8_t len = 10;
-    uint8_t gap_w = 2;
     switch (state_to_show) {
+        case SYMBOLS_ACTIVE:
+            oled_show(0, PSTR("SYMBOLS:"), gap_w, len, 8, true);
+            if (layer_state_is(SYMBOLS)) {
+                oled_show(1, PSTR("ON"), gap_w + 10, len - 1, 2, true);
+            } else {
+                oled_show(1, PSTR("OFF"), gap_w + 10, len - 1, 3, true);
+            }
+            return;
+        case LED_MATRIX_HUE:
+		case LED_MATRIX_SATURATION:
+        case LED_MATRIX_VOLUME:
+        case LED_MATRIX_MODE:
+    		uint8_t led_matrix_value_to_show = led_matrix_state_value();
+            static char led_matrix_value_line[9];
+            int led_matrix_actual_length = sprintf(led_matrix_value_line, "%d", led_matrix_value_to_show);
+            oled_show(1, led_matrix_value_line, gap_w + 10, len - 1, led_matrix_actual_length, false);
+            return;
 #ifdef KEY_OVERRIDE_ENABLE
         case KEY_OVERRIDES:
-            bool en = key_override_is_enabled();
             oled_show(0, PSTR("KEY OVRDS:"), gap_w, len, 10, true);
-            if (en) {
+            if (key_override_is_enabled()) {
                 oled_show(1, PSTR("ON"), gap_w + 10, len - 1, 2, true);
             } else {
                 oled_show(1, PSTR("OFF"), gap_w + 10, len - 1, 3, true);
@@ -276,7 +329,7 @@ void render_layer(uint8_t layer) {
             oled_show(1, PSTR("3:CONTROL"), gap_w, len, 9, true);
         break;
         case 4:
-            oled_show(1, PSTR("4:UNUSED"), gap_w, len, 8, true);
+            oled_show(1, PSTR("4:UNICODE"), gap_w, len, 9, true);
         break;
         default:
             oled_show(1, PSTR("UNKNOWN"), gap_w, len, 7, true);
@@ -295,24 +348,43 @@ bool oled_task_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!(record->event.pressed)) {
+        return true;
+    }
     switch (keycode) {
+        case KC_LSFT:
+        case KC_RSFT:
+            state_to_show = SYMBOLS_ACTIVE;
+        	return true;
+        case RM_HUED:
+        case RM_HUEU:
+            state_to_show = LED_MATRIX_HUE;
+        	return true;
+        case RM_SATD:
+        case RM_SATU:
+            state_to_show = LED_MATRIX_SATURATION;
+        	return true;
+        case RM_VALD:
+        case RM_VALU:
+            state_to_show = LED_MATRIX_VOLUME;
+        	return true;
+        case RM_PREV:
+        case RM_NEXT:
+            state_to_show = LED_MATRIX_MODE;
+        	return true;
 #ifdef KEY_OVERRIDE_ENABLE
         case KO_TOGG:
         case KO_ON:
         case KO_OFF:
-            if (record->event.pressed) {
-                state_to_show = KEY_OVERRIDES;
-            }
-        return true;
+            state_to_show = KEY_OVERRIDES;
+        	return true;
 #endif
 #ifdef DYNAMIC_TAPPING_TERM_ENABLE
         case DT_PRNT:
         case DT_UP:
         case DT_DOWN:
-            if (record->event.pressed) {
-                state_to_show = DT_TERM;
-            }
-        return true;
+            state_to_show = DT_TERM;
+        	return true;
 #endif
         default:
             return true;
